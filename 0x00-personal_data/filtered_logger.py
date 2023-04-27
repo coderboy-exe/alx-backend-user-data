3205edb77562a138aa5bf591967d0fbb88a33a46
Uchechukwu Ottah <usottah@gmail.com>
2023-04-27 13:04:27 +0000

    Update: It was the darn type-annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """ Module definition """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ Returns obfuscated log message """
     for field in fields:
-        pattern = fr'({field})=([^{separator}]*)'
-        message = re.sub(pattern, fr'\1={redaction}', message)
+        message = re.sub(fr'({field})=([^{separator}]*)', fr'\1={redaction}',
+                         message)
     return message
