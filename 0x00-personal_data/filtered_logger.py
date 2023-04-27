4919933f51912e38b5c11ea2a49333594873ab00
Uchechukwu Ottah <usottah@gmail.com>
2023-04-27 12:24:45 +0000

    Document functions

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,15 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
+    """
+        Returns obfuscated log message:
+        Arguments:
+            fields: a list of strings representing all fields
+            redaction:
+            message:
+            separator:
+    """
+
     pattern = '|'.join(fields)
     r = re.sub(fr'({pattern})=([^{separator}]*)', fr'\1={redaction}', message)
     return r
