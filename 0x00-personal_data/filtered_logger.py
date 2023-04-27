eafdd4a6602985b4a53a63408a6ca4a759036779
Uchechukwu Ottah <usottah@gmail.com>
2023-04-27 13:29:12 +0000

    Update: format incomint log records

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Module definition """
 import re
+import logging
 from typing import List
 
 
@@ -11,3 +12,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(fr'({field})=([^{separator}]*)', fr'\1={redaction}',
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ class initializaton method """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter and format values in incomin log records """
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message,
+                            self.SEPARATOR)
