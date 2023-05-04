5e9d91ada7f15e55fc04f18567ce2bb92c4679ba
Uchechukwu Ottah <usottah@gmail.com>
2023-05-04 21:50:40 +0000

    Updates: Identify User by session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,3 +25,10 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns the current User ID based on a cookie value """
+        session = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session)
+
+        return User.get(user_id)
