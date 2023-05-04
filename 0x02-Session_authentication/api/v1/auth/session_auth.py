62e2c137c0205afd0dbf9303041c65d05a8c36fc
Uchechukwu Ottah <usottah@gmail.com>
2023-05-04 18:41:52 +0000

    Update: session creation via session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,18 @@
 from .auth import Auth
 from models.user import User
 from typing import TypeVar
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Auth class which inherits from Auth class """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a new session ID for a user_id """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
