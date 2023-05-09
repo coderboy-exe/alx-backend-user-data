325b1ce922cdb9cb70dde38787494258f23a6c90
Uchechukwu Ottah <usottah@gmail.com>
2023-05-09 16:58:24 +0000

    create user | find user (using filter_by) | update user using passed kwargs

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,70 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+
+from user import Base, User
+
+from typing import TypeVar
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """
+            Saves the user in the database and returns the User object
+            Args:
+                email: string,
+                password: string,
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> TypeVar('User'):
+        """
+            Returns the first row found in the users table as filtered
+            by the input arguments
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+            Updates a user's attribute and returns None
+            -   Uses find_user_by to locate the user to update
+        """
+        user = self.find_user_by(id=user_id)
+        if not user:
+            raise ValueError
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+        self._session.commit()
