4ce8fbd7c99f95440145d18acfacde0d87a41f14
Uchechukwu Ottah <usottah@gmail.com>
2023-05-10 14:39:00 +0000

    Update: Generate session }| destroy session | login | logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ App module (entry point) """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, redirect
 
 from auth import Auth
 
@@ -30,6 +30,41 @@ def users() -> str:
         return jsonify(email=user.email, message="user created")
     except ValueError:
         return jsonify(message="email already registered"), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ Login handler """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.valid_login(email, password)
+        if user:
+            session_id = AUTH.create_session(email)
+            response = jsonify(email=email, message="logged in")
+            response.set_cookie("session_id", session_id)
+            return response
+        else:
+            abort(401)
+    except Exception:
+        abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ Logout handler """
+    try:
+        cookie = request.headers.get('session_id')
+        user = AUTH.get_user_from_session_id(cookie)
+
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect('/')
+        else:
+            abort(403)
+    except Exception:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Auth class module """
 import bcrypt
+import uuid
 
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -13,6 +14,11 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     password_bytes = password.encode('utf-8')
     return bcrypt.hashpw(password_bytes, salt)
+
+
+def _generate_uuid() -> str:
+    """ Returns a stringified uuid """
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -39,3 +45,43 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+            Checks password to see if ti matches password hash for
+            a given email.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            encoded = password.encode('utf-8')
+            return bcrypt.checkpw(encoded, user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """ Takes an email argument and returns the session ID """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ Returns the user for a corresponding session_id """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: str) -> None:
+        """ Destroys a user session. Returns Nothing """
+        try:
+            self._db.update_user(user_id, session_id=None)
+            return None
+        except ValueError:
+            pass
